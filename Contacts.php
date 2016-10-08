7799f798c9a11a60c0a9e073522e843ff86d8a2b
Manraj Singh <manrajsinghgrover@gmail.com>
2016-10-08 13:52:58 +0530

    Editorconfig added

--- Contacts.php
+++ Contacts.php
@@ -1,2 +1,31 @@
 <?php
+    class Contacts {
+        private $firstName;
+        private $lastName;
+        private $phoneNumber;
 
+        public function getFirstName() {
+            return $this->firstName;
+        }
+
+        public function setFirstName($_firstName) {
+            $this->firstName = $_firstName;
+        }
+
+        public function getLastName() {
+            return $this->lastName;
+        }
+
+        public function setLastName($_lastName) {
+            $this->lastName = $_lastName;
+        }
+
+        public function getPhoneNumber() {
+            return $this->phoneNumber;
+        }
+
+        public function setPhoneNumber($_phoneNumber) {
+            $this->firstName = $_phoneNumber;
+        }
+    }
+?>
