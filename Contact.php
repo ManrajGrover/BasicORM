4447727de02ff79d500d3c75be87ea875fe2f351
Manraj Singh <manrajsinghgrover@gmail.com>
2016-10-09 22:49:53 +0530

    Comments for Contact.php added

--- Contact.php
+++ Contact.php
@@ -16,26 +16,50 @@
             $this->phoneNumber = $_phoneNumber;
         }
 
+        /**
+         * Gets first name of contact
+         * @return String First name of contact
+         */
         public function getFirstName() {
             return $this->firstName;
         }
 
+        /**
+         * Sets first name of contact
+         * @param String $_firstName First name to be set
+         */
         public function setFirstName($_firstName) {
             $this->firstName = $_firstName;
         }
 
+        /**
+         * Gets last name of contact
+         * @return String Last name of contact
+         */
         public function getLastName() {
             return $this->lastName;
         }
 
+        /**
+         * Sets last name of contact
+         * @param String $_lastName Last name to be set
+         */
         public function setLastName($_lastName) {
             $this->lastName = $_lastName;
         }
 
+        /**
+         * Gets phone number of contact
+         * @return String Phone number of contact
+         */
         public function getPhoneNumber() {
             return $this->phoneNumber;
         }
 
+        /**
+         * Sets phone number of contact
+         * @param String $_phoneNumber Phone number to be set
+         */
         public function setPhoneNumber($_phoneNumber) {
             $this->firstName = $_phoneNumber;
         }
